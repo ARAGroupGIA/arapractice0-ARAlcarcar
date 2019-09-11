4dfb4824cdd23513f37989a36f240c5ed42beab5
ARAlcarcar <55189102+ARAlcarcar@users.noreply.github.com>
2019-09-11 14:38:30 +0200

    Exercise 2 - version 1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+main()
+{
+ int a,b,c;
+ printf ("Entry your firsr number");
+ scanf ("%f",&a);
+ printf ("Entry your second number");
+ scanf ("%f",&b);
+ c=a+b ;
+ printf("The sum of both numbers is: %f",&c);
+}
